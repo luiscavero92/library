0eb2640b7b22aa5d7fe4d2a55c85a5a4a02cd2ed
luiscavero92 <luiscavero92@gmail.com>
2016-06-01 21:41:34 +0200

    Loan copy security

--- src/AppBundle/Controller/v1/LoanController.php
+++ src/AppBundle/Controller/v1/LoanController.php
@@ -2,6 +2,7 @@
 namespace AppBundle\Controller\v1;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class LoanController extends RESTParentController
 {
@@ -26,7 +27,9 @@ class LoanController extends RESTParentController
         $validItem = $this->validateWithForm(new $this->fullEntityName, $request);
 
         $validItem->setLoanDate(new \DateTime());
-
+        if(!$validItem->getCopy()->getAvailable()){
+            throw new HttpException(400, 'This copy is not currently available');
+        }
         $this->persistItem($validItem);
 
         return $this->returnView($validItem, 201);   
@@ -34,7 +37,21 @@ class LoanController extends RESTParentController
 
     public function patchLoansAction(Request $request, $id)
     {
-        return parent::patch($request, $id);  
+        $item = $this->getOneById($id);
+
+        if(!$item){
+            throw new HttpException(404, $this->noItemFoundMsg);
+        }
+
+        $validItem = $this->validateWithForm($item, $request, true);
+
+        if(!$validItem->getCopy()->getAvailable()){
+            throw new HttpException(400, 'This copy is not currently available');
+        }
+        
+        $this->updateItem($validItem);
+
+        return $this->returnView($validItem, 200); 
     }
 
     public function deleteLoansAction($id)
